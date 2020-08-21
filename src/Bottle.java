7551236c3b58e91b4f28dab5397df081ba214f1b
rafalmoczydlowski <moczydlowski7@wp.pl>
2020-08-21 08:37:22 +0200

    Define a constant instead of duplicating literal

--- src/Bottle.java
+++ src/Bottle.java
@@ -1,8 +1,14 @@
 public class Bottle {
+
     private double capacity;
     private double amountOfWater;
     int bottleId = 0;
+
     private static final String AFTER_THIS = "After this, the water capacity of the bottle ";
+    private static final String POUR_OUT = "You are trying to pour out too much water!!! You can pour out ";
+    private static final String POUR_OUT_2 = " and you are trying to pour out ";
+    private static final String POUR = "You are trying to pour too much!!! You can pour ";
+    private static final String WATER = " water from bottle ";
 
     Bottle(double capacity, int bottleId) {
         this.bottleId = bottleId;
@@ -11,45 +17,40 @@ public class Bottle {
         System.out.println("The capacity of bottle " + bottleId + " is " + capacity + ". There is " + amountOfWater + " water.");
     }
 
-    boolean pour(double refilled) {
-        System.out.println("You are trying to pour " + refilled + " water into a bottle " + this.bottleId + ". There is " + this.amountOfWater + " water.");
-        if (capacity >= amountOfWater + refilled) {
-            amountOfWater += refilled;
-            System.out.println(AFTER_THIS + this.bottleId + " is: " + amountOfWater);
+    boolean pour(double quantity) {
+        System.out.println("You are trying to pour " + quantity + " water into a bottle " + this.bottleId + ". There is " + this.amountOfWater + " water.");
+        if (this.capacity >= this.amountOfWater + quantity) {
+            this.amountOfWater += quantity;
+            System.out.println(AFTER_THIS + this.bottleId + " is: " + this.amountOfWater);
             return true;
         }
         else
-            System.out.println("You are trying to pour too much!!! You can pour " + (capacity - amountOfWater));
+            System.out.println(POUR + (capacity - amountOfWater));
             return false;
     }
     boolean pourOut(double quantity) {
         System.out.println("You are trying to pour out " + quantity + " water into a bottle " + this.bottleId + ". There is " + this.amountOfWater + " water.");
-        if (amountOfWater > quantity) {
-            amountOfWater -= quantity;
-            System.out.println(AFTER_THIS + this.bottleId + " is: " + amountOfWater);
+        if (this.amountOfWater >= quantity) {
+            this.amountOfWater -= quantity;
+            System.out.println(AFTER_THIS + this.bottleId + " is: " + this.amountOfWater);
             return true;
         }
         else
-            System.out.println("You are trying to pour out too much water!!! You can pour out " + (this.capacity - this.amountOfWater) + " water from bottle " + this.bottleId + " and you are trying to pour out " + quantity + " water.");
+            System.out.println(POUR_OUT + this.amountOfWater + WATER + this.bottleId + POUR_OUT_2 + quantity + " water.");
             return false;
     }
-    boolean pourOver(Bottle bottle, double quantity) {
-        System.out.println("You are trying to pour over " + quantity + " water from bottle " + this.bottleId + " into bottle " + bottle.bottleId + ".");
-        if (this.amountOfWater > quantity && bottle.capacity >= bottle.amountOfWater + quantity) {
-            this.amountOfWater -= quantity;
-            bottle.amountOfWater += quantity;
-            System.out.println(AFTER_THIS + this.bottleId + " is " + this.amountOfWater + " and the water capacity of the bottle " + bottle.bottleId + " is " + bottle.amountOfWater);
-            return true;
+    void pourOver(Bottle destBottle, double quantity) {
+        System.out.println("You are trying to pour over " + quantity + WATER + this.bottleId + " into bottle " + destBottle.bottleId + ".");
+        if (this.amountOfWater >= quantity && destBottle.capacity >= destBottle.amountOfWater + quantity) {
+            this.pourOut(quantity);
+            destBottle.pour(quantity);
+        }
+        else if (destBottle.capacity < destBottle.amountOfWater + quantity) {
+            System.out.println(POUR + (destBottle.capacity - destBottle.amountOfWater) + " water into bottle " + destBottle.bottleId);
         }
         else if (this.amountOfWater < quantity) {
-            System.out.println("You are trying to pour out too much water!!! You have " + this.amountOfWater + " water in bottle " + this.bottleId + " and you are trying to pour out " + quantity);
-            return false;
+            System.out.println(POUR_OUT + this.amountOfWater + WATER + this.bottleId + POUR_OUT_2 + quantity + " water.");
         }
-        else if (bottle.capacity < bottle.amountOfWater + quantity) {
-            System.out.println("You are trying to pour too much!!! You can pour " + (bottle.capacity - bottle.amountOfWater) + " to bottle " + bottle.bottleId);
-            return false;
-        }
-        else return false;
     }
 
     public double getAmountOfWater() {
--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,9 @@ public class Main {
         Bottle bottle1 = new Bottle(1.5, 1);
         Bottle bottle2 = new Bottle(3.0, 2);
 
-        bottle1.pour(1.0);
-        bottle2.pour(3.0);
-        bottle2.pourOut(0.1);
-        bottle2.pourOver(bottle1, 0.5);
-
+        bottle1.pour(1.5);
+        bottle2.pour(1.5);
+        bottle2.pourOver(bottle1,1.5);
         bottle1.getAmountOfWater();
         bottle2.getAmountOfWater();
     }
